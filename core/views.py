359fdd70268b7f7565996540164ceed96d00c022
Abdul Magomedov <privetabdul@gmail.com>
2019-11-25 13:03:58 +0300

    created home, registration, login, logout pages

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'core/home.html')
+
+
+def register(request):
+    form = UserRegistrationForm
+    if request.method != 'POST':
+        form = UserRegistrationForm()
+    else:
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            authenticated_user = authenticate(
+                username=username, password=raw_password,
+            )
+            login(request, authenticated_user)
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'core/register.html', context)
